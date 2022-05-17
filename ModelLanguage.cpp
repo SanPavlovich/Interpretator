d53a4134e937bde232e0e0ea173a78e67f4c63ec
SanPavlovich <101992475+SanPavlovich@users.noreply.github.com>
2022-05-17 23:01:43 +0300

    Update ModelLanguage.cpp

--- ModelLanguage.cpp
+++ ModelLanguage.cpp
@@ -1,37 +1,12 @@
-﻿#include <iostream>
+#include <iostream>
 #include "Interpretator.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    /*Scanner a("C:\\Prak\\Cplus\\TestCode.txt");
-    int i = 0;
-    while (true) {
-        Lex lexemma = a.get_lex();
-        cout << i << ": " << lexemma << "\n";
-        if (lexemma.get_type() == LEX_FIN)
-            break;
-        i++;
-    }
-    cout << "our TID:" << "\n";
-    vector<Ident> b = a.getTID();
-    for (i = 0; i < b.size(); i++)
-        cout << b[i].get_name() << " "
-        << b[i].get_assign() << " "
-        << b[i].get_declare() << " " 
-        << b[i].get_type() << " " 
-        << b[i].get_value() << "\n";
-    */
-
-    Interpretator I("C:\\Prak\\Cplus\\TestCode.txt");
-    I.interpretation();
-    //I.test_parsing();
-    return 0;
-
-    /*try {
+    try {
         Interpretator I("C:\\Prak\\Cplus\\TestCode.txt");
-        //I.interpretation();
-        I.test_parsing();
+        I.interpretation();
+        //I.test_parsing();
         return 0;
     }
     catch (char c) {
@@ -45,6 +20,7 @@ int main()
     catch (const char* source) {
         cout << source << endl;
         return 1;
-    }*/
+    }
 }
+
 
